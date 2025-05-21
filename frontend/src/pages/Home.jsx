4d50732ab4d041af03edeb038b54dfd1a430d99e
Ajay <ajayu.canada@gmail.com>
2025-05-21 01:06:43 -0700

    Set up basic page routing

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Hello from Home</h1>
+      <p>If you see this, routing works!</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/Tracker.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Tracker() {
+  return (
+    <div>
+      <h1>Tracker Page</h1>
+      <p>This is the tracker page.</p>
+    </div>
+  )
+}
+
+export default Tracker
