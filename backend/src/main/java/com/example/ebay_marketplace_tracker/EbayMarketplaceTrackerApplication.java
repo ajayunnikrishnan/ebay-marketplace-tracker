b4909eedc0fb0c892bb73dabcd4a58e0b5939518
Ajay <ajayu.canada@gmail.com>
2025-05-21 01:12:07 -0700

    Add scaffold backend

---
+++ backend/src/main/java/com/example/ebay_marketplace_tracker/EbayMarketplaceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebay_marketplace_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayMarketplaceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayMarketplaceTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/ebay_marketplace_tracker/EbayMarketplaceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebay_marketplace_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbayMarketplaceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
