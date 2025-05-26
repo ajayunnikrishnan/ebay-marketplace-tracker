7bc581551a02f08e61758a98d7ad14b8c308379a
Ajay <ajayu.canada@gmail.com>
2025-05-26 00:49:15 -0700

    Add new SPA layout, and Navbar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "./components/Layout";
+import { Hero } from "./components/sections/Hero";
+
+function App() {
+	return <Layout title="EbMT Home"> 
+	</Layout>;
+}
+
+export default App;
